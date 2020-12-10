f1b6e67621825fc655e3acf3d3270ce349447564
marketmix-valenciad <duvan.valencia@inoutdelivery.com>
2020-12-10 10:41:03 -0500

    Agregando salto de linea con una coma

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -7,21 +7,24 @@ class Product {
 
 List<Product> products = [
   Product(
-      id: 1,
-      price: 56,
-      title: 'clasicc leather arm chair',
-      image: 'assets/images/Item_1.png',
-      description: 'Lorem ipsum dolor sit amet'),
+    id: 1,
+    price: 56,
+    title: 'clasicc leather arm chair',
+    image: 'assets/images/Item_1.png',
+    description: 'Lorem ipsum dolor sit amet',
+  ),
   Product(
-      id: 2,
-      price: 68,
-      title: 'clasicc leather arm chair',
-      image: 'assets/images/Item_2.png',
-      description: 'Lorem ipsum dolor sit amet'),
+    id: 2,
+    price: 68,
+    title: 'clasicc leather arm chair',
+    image: 'assets/images/Item_2.png',
+    description: 'Lorem ipsum dolor sit amet',
+  ),
   Product(
-      id: 3,
-      price: 39,
-      title: 'clasicc leather arm chair',
-      image: 'assets/images/Item_3.png',
-      description: 'Lorem ipsum dolor sit amet')
+    id: 3,
+    price: 39,
+    title: 'clasicc leather arm chair',
+    image: 'assets/images/Item_3.png',
+    description: 'Lorem ipsum dolor sit amet',
+  )
 ];
