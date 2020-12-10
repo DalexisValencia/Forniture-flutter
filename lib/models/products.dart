68cffef2ab32469ed88addc3dcbe99df92314f0d
marketmix-valenciad <duvan.valencia@inoutdelivery.com>
2020-12-10 12:01:16 -0500

    Agregando iconos y ajustes para construir el apk

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -2,7 +2,13 @@ class Product {
   final int id, price;
   final String title, description, image;
 
-  Product({this.id, this.price, this.title, this.description, this.image});
+  Product({
+    this.id,
+    this.price,
+    this.title,
+    this.description,
+    this.image,
+  });
 }
 
 List<Product> products = [
